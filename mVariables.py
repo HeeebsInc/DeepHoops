761c9d6a8646f81d8a0368a4559254868dec4ae3
HeeebsInc <60490377+HeeebsInc@users.noreply.github.com>
2020-06-02 14:47:54 -0400

    Add files via upload

---
+++ mVariables.py
@@ -0,0 +1,12 @@
+team_dictionary = {
+    'Atlanta Hawks': 'Atl', 'Boston Celtics': 'Bos', 'Brooklyn Nets': 'Bkn', 'Charlotte Hornets': 'Cha', 'Chicago Bulls': 'Chi', 'Cleveland Cavaliers': 'Cle', 'Dallas Mavericks': 'Dal',
+    'Denver Nuggets': 'Den', 'Detroit Pistons': 'Det','Golden State Warriors': 'GSW', 'Houston Rockets': 'Hou', 'Indiana Pacers': 'Ind',
+    'Los Angeles Lakers': 'LAL', 'Los Angeles Clippers': 'LAC', 'Memphis Grizzlies': 'Mem', 'Miami Heat': 'Mia', 'Milwaukee Bucks': 'Mil',
+    'Minnesota Timberwolves': 'Min', 'New Orleans Pelicans': 'Nor', 'New York Knicks': 'NYK', 'Oklahoma City Thunder': 'OKC', 'Orlando Magic': 'Orl', 'Philadelphia 76ers': 'Phi',
+    'Phoenix Suns': 'Pho', 'Portland Trail Blazers': 'Por', 'Sacramento Kings': 'Sac', 'San Antonio Spurs': 'SAS', 'Toronto Raptors': 'Tor', 'Utah Jazz': 'Uta', 'Washington Wizards': 'Was'
+}
+
+month_dictionary = {'Jan': '01', 'Feb': '02',  'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+
+
+month_list = ['september','october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june']
